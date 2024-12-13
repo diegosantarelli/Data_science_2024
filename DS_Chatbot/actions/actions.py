853a9968de46dd486248761ead92a86ce4cd5600
SimoneRecinelli <simonerecinelli@gmail.com>
2024-12-13 12:09:40 +0100

    updated chatbot

--- DS_Chatbot/actions/actions.py
+++ DS_Chatbot/actions/actions.py
@@ -25,3 +25,37 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+import requests
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionFetchCompetitions(Action):
+    def name(self) -> Text:
+        return "action_fetch_competitions"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Token personale
+        API_TOKEN = "728961a145e54aa18538db538fe9d634"
+        BASE_URL = "https://api.football-data.org/v4"
+        endpoint = "/competitions"
+
+        # Effettua la richiesta
+        url = f"{BASE_URL}{endpoint}"
+        headers = {"X-Auth-Token": API_TOKEN}
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            competitions = [comp['name'] for comp in data['competitions']]
+            message = "Ecco tutte le competizioni disponibili:\n" + "\n".join(competitions)
+        else:
+            message = f"Errore nel recuperare i dati: {response.status_code}"
+
+        # Invia la risposta al chatbot
+        dispatcher.utter_message(text=message)
+        return []
